4c15e864688e968f4dbc72c6175b41845a42fade
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-09-07 23:31:49 -0500

    in Podman

---
+++ src/in Podman/cvernas buildah/run.sh
@@ -0,0 +1,9 @@
+podman run --rm -it \
+--volume /run/libvirt:/run/libvirt \
+--volume "${HOME}:${HOME}:rslave" \
+--env "HOME=${HOME}" \
+--workdir "$(pwd)" \
+--net host \
+--privileged \
+--security-opt label=disable \
+localhost/vagrant:latest
